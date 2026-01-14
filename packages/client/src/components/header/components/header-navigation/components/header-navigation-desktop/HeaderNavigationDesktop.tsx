30db9911e02657b58e3b0582bac1b64d56feab81
martinezdylanok <dylan@martinezdylan.dev>
2026-01-14 11:57:38 +0100

    feat: add HeaderNavigationDesktop component with styles and data
    - Introduced HeaderNavigationDesktop component for desktop navigation.
    - Created associated data file for navigation links and ARIA labels.
    - Added CSS animations for menu opening and closing transitions.

---
+++ packages/client/src/components/header/components/header-navigation/components/header-navigation-desktop/HeaderNavigationDesktop.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./styles/default-navigation-animations.css";
+
+import { HEADER_DESKTOP_NAVIGATION_LINKS, LIST_ARIA_LABEL_TEXT, NAV_ARIA_LABEL_TEXT } from "./data/headerNavigationDesktopData";
+
+const HeaderNavigationDesktop = (): React.ReactElement => {
+   return (
+      <nav className="header-navigation-desktop hidden lg:flex items-center" aria-label={NAV_ARIA_LABEL_TEXT}>
+         <ul className="header-navigation-desktop__list flex justify-center gap-5" aria-label={LIST_ARIA_LABEL_TEXT}>
+            {HEADER_DESKTOP_NAVIGATION_LINKS.map((link, index) => (
+               <li key={index} className="header-navigation-desktop__item">
+                  <a href={link.HREF} className="header-navigation-desktop__link">
+                     <span className="header-navigation-desktop__link-text text-heading hover:text-accent font-hanken-grotesk font-bold text-xl transition-colors duration-300 ease-in-out cursor-pointer">{link.LABEL}</span>
+                  </a>
+               </li>
+            ))}
+         </ul>
+      </nav>
+   );
+};
+
+export default HeaderNavigationDesktop;
