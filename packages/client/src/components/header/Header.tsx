75b87db1db64a51a04d721295aa01e7f2d2a48d4
martinezdylanok <dylan@martinezdylan.dev>
2026-01-14 11:47:45 +0100

    refactor: improve Header component structure and styling
    - Updated Header component to use TypeScript for better type safety.
    - Simplified class names for improved readability and maintainability.
    - Removed unused ARIA label and adjusted layout for better responsiveness.

--- packages/client/src/components/header/Header.tsx
+++ packages/client/src/components/header/Header.tsx
@@ -1,25 +1,25 @@
+import React from "react";
 import { useHeaderVisibility } from "../../utils/hooks/useHeaderVisibility/useHeaderVisibility";
 import HeaderLogo from "./components/header-logo/HeaderLogo";
 import HeaderModeSwitcher from "./components/header-mode-switcher/HeaderModeSwitcher";
 import HeaderNavigation from "./components/header-navigation/HeaderNavigation";
-import { HEADER_ARIA_LABEL } from "./data/headerData";
 
-const Header = () => {
+const Header = (): React.ReactElement => {
    const isVisible = useHeaderVisibility();
 
    return (
       <header
-         aria-label={HEADER_ARIA_LABEL}
          className={`
             header
-            fixed w-full z-9999 
+            fixed
+            w-full
+            z-[9999] 
             bg-page
             transition-transform duration-600 ease-in-out
             ${isVisible ? "transform-none" : "-translate-y-full"}
          `}
       >
-         {/* TODO: Modify the header animation duration, also check if motion animation can be applied */}
-         <div className="w-full lg:max-w-7xl flex items-center justify-between p-6 mx-auto">
+         <div className="flex items-center justify-between p-6 px-auto">
             <HeaderLogo />
             <HeaderNavigation />
             <HeaderModeSwitcher />
