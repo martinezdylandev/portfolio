31a2abe36a1cffeee39a0db94527072dd8ba69e2
martinezdylanok <dylan@martinezdylan.dev>
2026-01-27 13:33:58 +0100

    refactor: adjust order class for FooterContentLinks component to improve layout responsiveness

--- packages/client/src/components/footer/components/footer-content/components/footer-content-links/FooterContentLinks.tsx
+++ packages/client/src/components/footer/components/footer-content/components/footer-content-links/FooterContentLinks.tsx
@@ -4,7 +4,7 @@ import { FOOTER_CONTENT_LINKS__ARIA_LABEL } from "./data/footerContentLinksData"
 
 const FooterContentLinks = () => {
    return (
-      <div className="footer-content__links flex justify-around py-20" aria-label={FOOTER_CONTENT_LINKS__ARIA_LABEL}>
+      <div className="footer-content__links order-1 md:order-2 flex justify-around py-20" aria-label={FOOTER_CONTENT_LINKS__ARIA_LABEL}>
          <FooterContentLinksSocial />
          <FooterContentLinksNavigation />
       </div>
