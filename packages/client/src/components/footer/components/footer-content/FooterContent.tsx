504a57505154b06841a53eab5572f00cf883efa2
martinezdylanok <dylan@martinezdylan.dev>
2026-01-27 13:32:03 +0100

    refactor: update FooterContent component layout and navigation
    - Replaced FooterContentLinks with FooterContentNavigation for improved navigation structure.
    - Adjusted layout styles for better responsiveness and alignment across devices.

--- packages/client/src/components/footer/components/footer-content/FooterContent.tsx
+++ packages/client/src/components/footer/components/footer-content/FooterContent.tsx
@@ -1,13 +1,13 @@
-import FooterContentLinks from "./components/footer-content-links/FooterContentLinks";
 import FooterContentLogo from "./components/footer-content-logo/FooterContentLogo";
+import FooterContentNavigation from "./components/footer-content-navigation/FooterContentNavigation";
 import FooterContentScrollUp from "./components/footer-content-scroll-up/FooterContentScrollUp";
 import { FOOTER_CONTENT_ARIA_LABEL } from "./data/footerContentData";
 
 const FooterContent = () => {
    return (
-      <div className="footer__content-container mx-5 grid grid-cols-3" aria-label={FOOTER_CONTENT_ARIA_LABEL}>
+      <div className="footer__content-container flex items-center justify-between px-6 py-12 md:px-auto md:grid md:grid-cols-3" aria-label={FOOTER_CONTENT_ARIA_LABEL}>
          <FooterContentLogo />
-         <FooterContentLinks />
+         <FooterContentNavigation />
          <FooterContentScrollUp />
       </div>
    );
