6570a51f72021c66ad3b5d109c18a5ab341de65d
martinezdylanok <dylan@martinezdylan.dev>
2026-01-27 13:37:25 +0100

    refactor: update FooterContentLogo component for improved layout and responsiveness
    - Adjusted order class for better alignment in mobile views.
    - Modified image dimensions using clamp for responsive sizing.

--- packages/client/src/components/footer/components/footer-content/components/footer-content-logo/FooterContentLogo.tsx
+++ packages/client/src/components/footer/components/footer-content/components/footer-content-logo/FooterContentLogo.tsx
@@ -5,9 +5,9 @@ const FooterContentLogo = () => {
    const { theme } = useThemeContext();
 
    return (
-      <div className="footer-content__logo flex h-fit w-fit justify-self-center py-20" aria-label={FOOTER_CONTENT_LOGO__ARIA_LABEL}>
-         <a className="footer-content__logo-link inline-block self-center" href="/">
-            <img src={`${theme === "light" ? FOOTER_CONTENT_LOGO_LIGHT_MODE : FOOTER_CONTENT_LOGO_DARK_MODE}`} className={`footer-content__logo-image cursor-pointer transition-all duration-500 ease-in-out hover:scale-110`} alt={FOOTER_CONTENT_LOGO__IMAGE_ALT} />
+      <div className="footer-content__logo order-2 md:order-1 md:flex md:h-fit md:w-fit md:justify-self-center md:py-20" aria-label={FOOTER_CONTENT_LOGO__ARIA_LABEL}>
+         <a href="/" className="footer-content__logo-link inline-block self-center">
+            <img src={`${theme === "light" ? FOOTER_CONTENT_LOGO_LIGHT_MODE : FOOTER_CONTENT_LOGO_DARK_MODE}`} className={`footer-content__logo-image w-[clamp(4.5rem,22vw,6.25rem)] h-[clamp(2.25rem,11vw,3.125rem)] cursor-pointer transition-all duration-500 ease-in-out hover:scale-110`} alt={FOOTER_CONTENT_LOGO__IMAGE_ALT} />
          </a>
       </div>
    );
