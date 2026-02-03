aaeca09a7f95980288a9765f2bf0202aad214217
martinezdylanok <dylan@martinezdylan.dev>
2026-02-03 11:48:26 +0100

    refactor: improve type safety in ContactFormActualForm component
    - Updated ContactFormActualForm to use ActiveFormProps for better type definition.
    - Changed activeForm condition from "firstForm" to "defaultForm" for clarity.

--- packages/client/src/features/contact-form/components/contact-form-actual-form/ContactFormActualForm.tsx
+++ packages/client/src/features/contact-form/components/contact-form-actual-form/ContactFormActualForm.tsx
@@ -1,13 +1,14 @@
+import React from "react";
+import { ActiveFormProps } from "../../data/contactFormData";
 import ContactFormDefault from "./components/contact-form-default/ContactFormDefault";
 import ContactFormJob from "./components/contact-form-job/ContactFormJob";
-import type { ContactFormActualFormProps } from "./data/contactFormActualFormData";
 import { CONTACT_FORM_ACTUAL_FORM_WRAPPER_ARIA_LABEL } from "./data/contactFormActualFormData";
 import "./styles/contact-form-actual-form-styles.css";
 
-const ContactFormActualForm = ({ activeForm }: ContactFormActualFormProps) => {
+const ContactFormActualForm = ({ activeForm }: ActiveFormProps): React.ReactElement => {
    return (
-      <div className="contact-form__actual-form-wrapper px-6  md:px-12" aria-label={CONTACT_FORM_ACTUAL_FORM_WRAPPER_ARIA_LABEL}>
-         {activeForm === "firstForm" ? <ContactFormDefault activeForm={activeForm} /> : <ContactFormJob activeForm={activeForm} />}
+      <div className="contact-form__actual-form-wrapper px-6 md:px-12" aria-label={CONTACT_FORM_ACTUAL_FORM_WRAPPER_ARIA_LABEL}>
+         {activeForm === "defaultForm" ? <ContactFormDefault activeForm={activeForm} /> : <ContactFormJob activeForm={activeForm} />}
       </div>
    );
 };
