2796924136e1964378d2eefca8de55c8437a138c
martinezdylanok <dylan@martinezdylan.dev>
2026-02-03 11:12:17 +0100

    refactor: enhance Footer component for improved accessibility and structure
    - Added an id attribute to the footer element for better accessibility.
    - Updated the Footer component to explicitly define its return type as React.ReactElement.

--- packages/client/src/components/footer/Footer.tsx
+++ packages/client/src/components/footer/Footer.tsx
@@ -1,9 +1,10 @@
+import React from "react";
 import FooterContent from "./components/footer-content/FooterContent";
 import { FOOTER_ARIA_LABEL } from "./data/footerData";
 
-const Footer = () => {
+const Footer = (): React.ReactElement => {
    return (
-      <footer className="footer bg-page" aria-label={FOOTER_ARIA_LABEL}>
+      <footer id="footer" className="footer bg-page" aria-label={FOOTER_ARIA_LABEL}>
          <FooterContent />
       </footer>
    );
