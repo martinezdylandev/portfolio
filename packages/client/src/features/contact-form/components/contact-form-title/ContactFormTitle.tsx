ba48e33eb7c22502eb2afc45bba187e04d736ebe
martinezdylanok <dylan@martinezdylan.dev>
2026-02-03 12:01:02 +0100

    refactor: enhance type safety in ContactFormTitle component
    - Updated ContactFormTitle to explicitly define return type as React.ReactElement for improved type safety.
    - Added React import to ensure proper usage of JSX syntax.

--- packages/client/src/features/contact-form/components/contact-form-title/ContactFormTitle.tsx
+++ packages/client/src/features/contact-form/components/contact-form-title/ContactFormTitle.tsx
@@ -1,6 +1,7 @@
+import React from "react";
 import { CONTACT_FORM_TITLE } from "./data/contactFormTitleData";
 
-const ContactFormTitle = () => {
+const ContactFormTitle = (): React.ReactElement => {
    return <h2 className="text-8xl text-center md:text-[160px] md:text-left font-hanken-grotesk font-bold mb-12 text-heading contact-form__title">{CONTACT_FORM_TITLE}</h2>;
 };
 
