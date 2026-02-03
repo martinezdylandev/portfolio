906db45102ee2867d03bf8ee2b13c7c01ba87a83
martinezdylanok <dylan@martinezdylan.dev>
2026-02-03 12:08:21 +0100

    refactor: update ActiveForm type and enhance ContactForm state management
    - Changed ActiveForm type values from "firstForm" and "secondForm" to "defaultForm" and "jobForm" for improved clarity.
    - Introduced ActiveFormProps interface to encapsulate activeForm state.
    - Added ContactFormStateProps interface to include a handler for updating the active form state.

--- packages/client/src/features/contact-form/data/contactFormData.ts
+++ packages/client/src/features/contact-form/data/contactFormData.ts
@@ -1,3 +1,9 @@
 export const CONTACT_FORM_ARIA_LABEL: string = "Contact form container";
 
-export type ActiveForm = "firstForm" | "secondForm";
+export type ActiveForm = "defaultForm" | "jobForm";
+export interface ActiveFormProps {
+   activeForm: ActiveForm;
+}
+export interface ContactFormStateProps extends ActiveFormProps {
+   handleActiveFormUpdate: (newActiveForm: ActiveForm) => void;
+}
