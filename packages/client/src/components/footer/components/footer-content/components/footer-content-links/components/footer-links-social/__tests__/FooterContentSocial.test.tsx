261bd82d024500dbae7ce4d0a4bbcde985e1e66c
martinezdylanok <dylan@martinezdylan.dev>
2026-02-13 10:22:06 +0100

    refactor: improve FooterContentLinksSocial component with TypeScript and enhance tests for accessibility
    - Added TypeScript return type to FooterContentLinksSocial component for better type safety.
    - Updated tests to use ARIA labels and constants for improved accessibility.
    - Removed redundant tests related to theme modes.

--- packages/client/src/components/footer/components/footer-content/components/footer-content-links/components/footer-links-social/__tests__/FooterContentSocial.test.tsx
+++ packages/client/src/components/footer/components/footer-content/components/footer-content-links/components/footer-links-social/__tests__/FooterContentSocial.test.tsx
@@ -1,42 +1,25 @@
 import { render, screen } from "@testing-library/react";
+import { FOOTER_CONTENT_SOCIAL_ARIA_LABEL, FOOTER_CONTENT_SOCIAL_SPAN_TEXT } from "../data/footerContentSocialData";
 import FooterContentSocial from "../FooterContentLinksSocial";
-import { setupLightMode, setupDarkMode, resetModes } from "./test-utils/testUtils";
+import { resetModes, setupLightMode } from "./test-utils/testUtils";
 
 describe("FooterContentSocial tests", () => {
    beforeAll(() => {
-      vi.mock("../../../../../../../../../utils/hooks/useTheme");
-   });
-
-   beforeEach(() => {
-      setupLightMode();
-   });
-
-   afterAll(() => {
+      vi.mock("../../../../../../../../../utils/hooks/useThemeContext/useThemeContext.tsx");
+      vi.mock("../../../../../../../../../utils/hooks/useMediaQuery/useMediaQuery.tsx");
       resetModes();
+      setupLightMode();
    });
 
    test("renders FooterContentSocial element", () => {
       render(<FooterContentSocial />);
-      const FooterContentSocialElement = screen.getByLabelText("Footer social container");
+      const FooterContentSocialElement = screen.getByLabelText(FOOTER_CONTENT_SOCIAL_ARIA_LABEL);
       expect(FooterContentSocialElement).toBeInTheDocument();
    });
 
    test("renders FooterContentSocial span element", () => {
       render(<FooterContentSocial />);
-      const FooterContentSocialSpanElement = screen.getByText("Follow me on");
+      const FooterContentSocialSpanElement = screen.getByText(FOOTER_CONTENT_SOCIAL_SPAN_TEXT);
       expect(FooterContentSocialSpanElement).toBeInTheDocument();
-   });
-
-   test("renders FooterContentSocial span element light mode", () => {
-      render(<FooterContentSocial />);
-      const FooterContentSocialSpanElement = screen.getByText("Follow me on");
-      expect(FooterContentSocialSpanElement).toHaveClass("text-[#ABC4FF]");
-   });
-
-   test("renders FooterContentSocial span element dark mode", () => {
-      setupDarkMode();
-      render(<FooterContentSocial />);
-      const FooterContentSocialSpanElement = screen.getByText("Follow me on");
-      expect(FooterContentSocialSpanElement).toHaveClass("text-[#EDF2FB]");
    });
 });
