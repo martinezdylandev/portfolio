fc4007008c999fae07f8db72760b8a9106445294
martinezdylanok <dylan@martinezdylan.dev>
2026-02-13 10:24:44 +0100

    refactor: enhance FooterContentLinksSocialGithub component with TypeScript and improve tests for accessibility
    - Added TypeScript return type to FooterContentLinksSocialGithub component for better type safety.
    - Updated tests to utilize ARIA labels and constants for improved accessibility.
    - Refactored theme mode setup in tests for consistency and clarity.

--- packages/client/src/components/footer/components/footer-content/components/footer-content-links/components/footer-links-social/components/footer-content-social-github/__tests__/FooterContentSocialGithub.test.tsx
+++ packages/client/src/components/footer/components/footer-content/components/footer-content-links/components/footer-links-social/components/footer-content-social-github/__tests__/FooterContentSocialGithub.test.tsx
@@ -1,49 +1,45 @@
 import { render, screen } from "@testing-library/react";
+import { FOOTER_CONTENT_SOCIAL_GITHUB_ARIA_LABEL, GITHUB_LOGO_ALT, GITHUB_LOGO_DARK_MODE, GITHUB_LOGO_LIGHT_MODE, GITHUB_PROFILE_URL } from "../data/footerContentSocialGithubData";
 import FooterContentLinksSocialGithub from "../FooterContentLinksSocialGithub";
-import { setupLightMode, resetModes, setupDarkMode } from "./test-utils/testUtils";
+import { resetModes, setupDarkMode, setupLightMode } from "./test-utils/testUtils";
 
 describe("FooterContentLinksSocialGithub component tests", () => {
    beforeAll(() => {
-      vi.mock("../../../../../../../../../../../utils/hooks/useTheme");
-   });
-
-   beforeEach(() => {
-      setupLightMode();
-   });
-
-   afterAll(() => {
+      vi.mock("../../../../../../../../../../../utils/hooks/useThemeContext/useThemeContext.tsx");
+      vi.mock("../../../../../../../../../../../utils/hooks/useMediaQuery/useMediaQuery.tsx");
       resetModes();
+      setupLightMode();
    });
 
    test("renders the FooterContentLinksGithub div element", () => {
       render(<FooterContentLinksSocialGithub />);
-      const footerCOntentSocialGithubContainer = screen.getByLabelText("Github logo container");
+      const footerCOntentSocialGithubContainer = screen.getByLabelText(FOOTER_CONTENT_SOCIAL_GITHUB_ARIA_LABEL);
       expect(footerCOntentSocialGithubContainer).toBeInTheDocument();
    });
 
    test("renders the FooterContentLinksGithub a element with the correct href", () => {
       render(<FooterContentLinksSocialGithub />);
       const footerContentLinksSocialGithubLink = screen.getByRole("link");
-      expect(footerContentLinksSocialGithubLink).toHaveAttribute("href", "https://github.com/martinezdylanok");
+      expect(footerContentLinksSocialGithubLink).toHaveAttribute("href", GITHUB_PROFILE_URL);
       expect(footerContentLinksSocialGithubLink).toBeInTheDocument();
    });
 
    test("renders the FooterContentLinksGithub element", () => {
       render(<FooterContentLinksSocialGithub />);
-      const footerContentLinksSocialGithub = screen.getByAltText("Github logo");
+      const footerContentLinksSocialGithub = screen.getByAltText(GITHUB_LOGO_ALT);
       expect(footerContentLinksSocialGithub).toBeInTheDocument();
    });
 
    test("renders the FooterContentLinksGithub light mode element", () => {
       render(<FooterContentLinksSocialGithub />);
-      const footerContentLinksSocialGithubLightMode = screen.getByAltText("Github logo");
-      expect(footerContentLinksSocialGithubLightMode).toHaveAttribute("src", " /footer/github_logo_dark_mode.svg");
+      const footerContentLinksSocialGithubLightMode = screen.getByAltText(GITHUB_LOGO_ALT);
+      expect(footerContentLinksSocialGithubLightMode).toHaveAttribute("src", GITHUB_LOGO_LIGHT_MODE);
    });
 
    test("renders the FooterContentLinksGithub dark mode element", () => {
       setupDarkMode();
       render(<FooterContentLinksSocialGithub />);
-      const footerContentLinksSocialGithubDarkMode = screen.getByAltText("Github logo");
-      expect(footerContentLinksSocialGithubDarkMode).toHaveAttribute("src", " /footer/github_logo_light_mode.svg");
+      const footerContentLinksSocialGithubDarkMode = screen.getByAltText(GITHUB_LOGO_ALT);
+      expect(footerContentLinksSocialGithubDarkMode).toHaveAttribute("src", GITHUB_LOGO_DARK_MODE);
    });
 });
