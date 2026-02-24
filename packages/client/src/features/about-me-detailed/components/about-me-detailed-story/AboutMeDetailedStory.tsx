9abc465c2f9e5d471c40be09f81452d1ae63ee2c
martinezdylanok <dylan@martinezdylan.dev>
2026-02-24 17:31:03 +0100

    feat: implement responsive AboutMeDetailedStory component with new data structure
    - Refactored AboutMeDetailedStory to conditionally render desktop and compact views based on screen size.
    - Created a new data file for managing story-related texts and images.
    - Removed unused imports and animations to streamline the component.

--- packages/client/src/features/about-me-detailed/components/about-me-detailed-story/AboutMeDetailedStory.tsx
+++ packages/client/src/features/about-me-detailed/components/about-me-detailed-story/AboutMeDetailedStory.tsx
@@ -1,25 +1,12 @@
-import { motion } from "framer-motion";
-import { CATEGORY_DESCRIPTION, CATEGORY_NAME, STORY_IMAGES } from "../../../about-me/data/aboutMeData";
-import { useAboutMeDetailedStoryAnimations } from "./utils/useAboutMeDetailedStoryAnimations";
+import React from "react";
+import { useMediaQuery } from "../../../../utils/hooks/useMediaQuery/useMediaQuery";
+import AboutMeDetailedStoryCompact from "./components/about-me-detailed-story-compact/AboutMeDetailedStoryCompact";
+import AboutMeDetailedStoryDesktop from "./components/about-me-detailed-story-desktop/AboutMeDetailedStoryDesktop";
 
-const AboutMeDetailedStory = () => {
-   const { initial, whileInView, transition, viewport } = useAboutMeDetailedStoryAnimations();
+const AboutMeDetailedStory = (): React.ReactElement => {
+   const isDesktop = useMediaQuery("(min-width: 1024px)");
 
-   return (
-      <div className="about-me-detailed__story flex flex-col text-right py-25 bg-page">
-         <motion.div initial={initial} whileInView={whileInView} transition={transition} viewport={viewport} className="about-me-detailed__story-content flex flex-col gap-12.5 min-w-screen min-h-screen">
-            <h3 className="about-me-detailed__story-title font-hanken-grotesk text-[200px] text-heading font-bold leading-none">{CATEGORY_NAME[3]}</h3>
-            <div className="about-me-detailed__story-images-container flex flex-col gap-12.5 h-full self-center items-center justify-center">
-               <div className="about-me-detailed__story-images flex gap-12.5">
-                  {STORY_IMAGES.map((image) => (
-                     <img key={image.src} className="about-me-detailed__story-image max-w-48 max-h-42 blur-xs cursor-pointer" src={image.src} alt={image.alt} />
-                  ))}
-               </div>
-               <span className="about-me-detailed__story-description font-hanken-grotesk font-medium text-4xl text-accent">{CATEGORY_DESCRIPTION[2]}</span>
-            </div>
-         </motion.div>
-      </div>
-   );
+   return isDesktop ? <AboutMeDetailedStoryDesktop /> : <AboutMeDetailedStoryCompact />;
 };
 
 export default AboutMeDetailedStory;
