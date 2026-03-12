0d1e34321d7a0fedd565d60bff722a964f61e3f4
martinezdylanok <dylan@martinezdylan.dev>
2026-03-12 09:39:23 +0100

    refactor: replace ProjectResultsContainer with responsive components
    - Removed ProjectResultsContainer and its associated tests.
    - Introduced ProjectResultsContainerCompact and ProjectResultsContainerDesktop for responsive design.
    - Updated ProjectResults component to conditionally render the appropriate container based on the isDesktop prop.

--- packages/client/src/features/project/components/project-results/ProjectResults.tsx
+++ packages/client/src/features/project/components/project-results/ProjectResults.tsx
@@ -1,10 +1,11 @@
-import ProjectResultsContainer from "./components/project-results-container/ProjectResultsContainer";
+import ProjectResultsContainerCompact from "./components/project-results-container-compact/ProjectResultsContainerCompact";
+import ProjectResultsContainerDesktop from "./components/project-results-container-desktop/ProjectResultsContainerDesktop";
 import { PROJECT_RESULTS_ARIA_LABEL, ProjectResultsProps } from "./data/projectResultsData";
 
-const ProjectResults = ({ project }: ProjectResultsProps) => {
+const ProjectResults = ({ project, isDesktop }: ProjectResultsProps) => {
    return (
-      <section className="project__results min-w-screen bg-section p-25" aria-label={PROJECT_RESULTS_ARIA_LABEL}>
-         <ProjectResultsContainer project={project} />
+      <section className="project__results min-w-screen bg-section" aria-label={PROJECT_RESULTS_ARIA_LABEL}>
+         {isDesktop ? <ProjectResultsContainerDesktop project={project} /> : <ProjectResultsContainerCompact project={project} />}
       </section>
    );
 };
