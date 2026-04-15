b7810d100e5adcf66e9bc9b0107be77562b2b7de
martinezdylanok <dylan@martinezdylan.dev>
2026-04-15 15:09:19 +0200

    fix: update activeForm condition in getContactFormDirection utility

--- packages/client/src/features/contact-form/utils/getContactFormDirection.ts
+++ packages/client/src/features/contact-form/utils/getContactFormDirection.ts
@@ -1,5 +1,5 @@
 import type { ActiveForm } from "../data/contactFormData";
 
 export const getContactFormDirection = (activeForm: ActiveForm): "left" | "right" => {
-   return activeForm === "firstForm" ? "left" : "right";
+   return activeForm === "defaultForm" ? "left" : "right";
 };
