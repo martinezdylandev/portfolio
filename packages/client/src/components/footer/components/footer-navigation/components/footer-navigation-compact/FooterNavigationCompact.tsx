a53cc8ec8f60b78bb08af9e6f1426f507f1e1777
martinezdylanok <dylan@martinezdylan.dev>
2026-05-29 10:42:32 +0200

    feat: enhance footer navigation with improved animations and layout adjustments

--- packages/client/src/components/footer/components/footer-navigation/components/footer-navigation-compact/FooterNavigationCompact.tsx
+++ packages/client/src/components/footer/components/footer-navigation/components/footer-navigation-compact/FooterNavigationCompact.tsx
@@ -11,14 +11,14 @@ const FooterNavigationCompact = (): React.ReactElement => {
    const { isNavigationMenuOpen, handleClick, navRef, buttonRef } = useCompactFooterNavigationMenu();
 
    return (
-      <div className="relative">
+      <div className="">
          <button ref={buttonRef} className="footer__navigation-compact__menu-button w-10 h-10 flex items-center justify-center" onClick={handleClick} aria-label={BUTTON_ARIA_LABEL} aria-expanded={isNavigationMenuOpen} aria-controls="footer-navigation-menu">
-            <img src={theme === "light" ? "/header/hamburguer_icon_light_mode.svg" : "/header/hamburguer_icon_dark_mode.svg"} className={`footer__navigation-compact__menu-icon w-6 h-6 transition-transform duration-100 ease-in-out ${isNavigationMenuOpen ? "rotate-90" : "rotate-0"}`} alt={ICON_ALT_LABEL} />
+            <img src={theme === "light" ? "/header/hamburguer_icon_light_mode.svg" : "/header/hamburguer_icon_dark_mode.svg"} className={`footer__navigation-compact__menu-icon w-6 h-6 transition-transform duration-300 ease-in-out ${isNavigationMenuOpen ? "rotate-90" : "rotate-0"}`} alt={ICON_ALT_LABEL} />
          </button>
          <AnimatePresence>
             {isNavigationMenuOpen && (
-               <motion.nav ref={navRef} id="footer-navigation-menu" tabIndex={-1} variants={navMenuVariants} initial="closed" animate="open" exit="closed" transition={{ duration: 0.2, ease: "easeInOut" }} className="footer__navigation-compact absolute bottom-full mb-2" aria-label={NAV_ARIA_LABEL_TEXT}>
-                  <ul className="footer__navigation-compact__list flex flex-col items-center justify-center bg-accent gap-5 py-10 px-5 rounded-lg shadow-lg" aria-label={LIST_ARIA_LABEL_TEXT}>
+               <motion.nav ref={navRef} id="footer-navigation-menu" tabIndex={-1} variants={navMenuVariants} initial="closed" animate="open" exit="closed" transition={{ duration: 0.3, ease: "easeInOut" }} className="footer__navigation-compact absolute bottom-full left-0" aria-label={NAV_ARIA_LABEL_TEXT}>
+                  <ul className="footer__navigation-compact__list flex flex-col bg-card gap-5 p-5 rounded-br-lg rounded-tr-lg shadow-lg" aria-label={LIST_ARIA_LABEL_TEXT}>
                      {FOOTER_COMPACT_NAVIGATION_LINKS.map((link) => (
                         <li key={link.HREF} className="footer__navigation-compact__item">
                            <Link to={link.HREF} aria-label={link.ARIA_LABEL} onClick={handleClick} className="footer__navigation-compact__link">
